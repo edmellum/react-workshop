44cc619f7c761008c3d355893ce06fd8efd2ad2e
David Ed Mellum <david@edmellum.com>
2014-03-14 17:08:10 +0100

    Fix broken like solution

--- like/like-solution.jsx
+++ like/like-solution.jsx
@@ -1,23 +1,22 @@
 /** @jsx React.DOM */
 
-var Like = React.createClass({
-  getInitialState: function() {
-    return {
-      liked: this.props.defaultLiked
-    };
-  },
+(function() {
+  window.Like = React.createClass({
+    getInitialState: function() {
+      return {
+        liked: this.props.defaultLiked
+      };
+    },
 
-  clicked: function(event) {
-    event.preventDefault();
-    this.setState({liked: !this.state.liked});
-    // $.post('/like', {liked: this.state.liked})
-  },
+    clicked: function(event) {
+      event.preventDefault();
+      this.setState({liked: !this.state.liked});
+      // $.post('/like', {liked: this.state.liked})
+    },
 
-  render: function() {
-    var className = this.state.liked ? 'liked' : '';
-    return <a className={className} onClick={this.clicked} href="#">{this.props.text}</a>;
-  }
-});
-
-var container = document.querySelector('#react-goes-here');
-React.renderComponent(Like({text: 'Like this'}), container);
+    render: function() {
+      var className = this.state.liked ? 'liked' : '';
+      return <a className={className} onClick={this.clicked} href="#">{this.props.text}</a>;
+    }
+  });
+})();
